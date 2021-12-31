89202a855204496f3b9966cc9cb2ee019bb2a41e
ljy-code <ljy910626@gmail.com>
2021-12-31 17:43:42 +0800

    feat: add basic,typescript,vue package

---
+++ packages/all/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [
+    "@ljy-code/eslint-config-basic",
+    "@ljy-code/eslint-config-typescript",
+    "@ljy-code/eslint-config-vue",
+  ],
+};
---
+++ packages/basic/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:eslint-comments/recommended",
+  ],
+};
